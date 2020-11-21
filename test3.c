6164928a51ad57ea9d08b14eb2dba85239285500
mth764 <1242065877>
2020-11-21 17:03:05 +0800

    提升动手能力共同学习

---
+++ test3.c
@@ -0,0 +1,33 @@
+//输出100以内3的倍数
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			printf("%d\n", i);
+//	}
+//	return 0;
+//
+//}
+//比较两个数输出最大值
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//void ac_max(int x,int y)
+//{
+//	if (x >= y)
+//		printf("最大值为%d\n", x);
+//	else
+//		printf("最大值为%d\n", y);
+//}
+//int main()
+//{	
+//	int a;
+//	int b;
+//	printf("请输入两个需要比较的数\n");
+//	scanf("%d %d", &a, &b);
+//	ac_max(a, b);
+//	return 0;
+//}
