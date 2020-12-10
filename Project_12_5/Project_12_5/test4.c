15a7419dc34a99a3dee50342d528785cdb4a78f7
mth764 <1242065877@qq.com>
2020-12-10 23:57:05 +0800

    C代码练习

---
+++ Project_12_5/Project_12_5/test4.c
@@ -0,0 +1,13 @@
+//不创建临时变量完成两个整数交换
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 8;
+//	printf("交换前:a = %d b = %d\n", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("交换后:a = %d b = %d\n", a, b);
+//}
