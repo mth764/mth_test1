71da3cfa594c5cd163d7955c9fbe14b8878353af
mth764 <1242065877@qq.com>
2020-12-11 21:21:26 +0800

    C代码练习

---
+++ Project_12_5/Project_12_5/test5.c
@@ -0,0 +1,12 @@
+////通过移位<<求出2的n次方
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	while (scanf("%d", &m) != EOF)
+//	{
+//		printf("%d\n", 1 << m);
+//	}
+//	return 0;
+//}
