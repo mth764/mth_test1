9330e71859c0b9abf750834b0e69d34a46be6085
mth764 <1242065877@qq.com>
2020-12-16 22:26:39 +0800

    C代码练习

---
+++ Project_12_09_3/Project_12_09_3/test1.c
@@ -0,0 +1,23 @@
+//Ñî»ÔÈý½Ç
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int n = 4;
+//	int j = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			if (j == 1 || j == i )
+//			{
+//				printf("1 ");
+//			}
+//			else
+//				printf("%d ", i - 1);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
---
+++ Project_12_09_3/Project_12_09_3/test2.c
@@ -0,0 +1,18 @@
+//猜凶手
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void) {
+//	const char men[] = { 'a', 'b', 'c', 'd' };
+//	int a, b, c, d;
+//	for (int who = 0; who < 4; who++)//嫌疑犯四种情况
+//	{
+//		a = (who != 0);
+//		b = (who == 2);
+//		c = (who == 3);
+//		d = (who != 3);
+//		printf("%d %d %d %d\n", a, b, c, d);
+//		if ((a + b + c + d) == 3) 
+//			printf("suspect is %c\n", men[who]);
+//	}
+//	return 0;
+//}
