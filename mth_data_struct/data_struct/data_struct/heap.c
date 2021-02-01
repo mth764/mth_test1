3b4cbb8a3fc77f095b56c84760d50969435af40e
mth764 <1242065877@qq.com>
2021-02-01 23:49:06 +0800

    数据结构

--- mth_data_struct/data_struct/data_struct/heap.c
+++ mth_data_struct/data_struct/data_struct/heap.c
@@ -4,6 +4,17 @@ int main()
 {
 	Heap hp;
 	HPDataType arr[] = { 27, 15, 19, 18, 28, 34, 65, 49, 25, 37 };
-	HeapCreate(&hp, arr, sizeof(arr) / sizeof(arr[0]));
+	int n = sizeof(arr) / sizeof(arr[0]);
+	HeapCreate(&hp, arr, n);
+	//≥ı ºªØ
+	HeapInit(&hp, n);
+	//∂—≤Â
+	for (int i = 0; i < n; i++)
+	{
+		HeapPush(&hp, arr[i]);
+	}
+	HeapShow(&hp, 0,n);
+	HeapPop(&hp);
+	HeapShow(&hp, 0, n);
 	return 0;
 }
--- mth_data_struct/data_struct/data_struct/heap.h
+++ mth_data_struct/data_struct/data_struct/heap.h
@@ -9,12 +9,14 @@ typedef struct Heap
 	int _size;
 	int _capacity;
 }Heap;
-
+//上调
+void _HeapUp(Heap* hp);
+//下调
+void _HeapDown(Heap* hp);
+//堆的初始化
+void HeapInit(Heap* hp,int n);
 // 堆的构建
 void HeapCreate(Heap* hp, HPDataType* a, int n);
-{
-
-}
 // 堆的销毁
 void HeapDestory(Heap* hp);
 // 堆的插入
@@ -38,12 +40,102 @@ void HeapSort(int* a, int n);
 // 找最小的前K个，建立K个数的大堆
 void PrintTopK(int* a, int n, int k);
 void TestTopk();
+void HeapShow(Heap* hp,int start,int end);
 
 
 //////////////////////////////////////////////////
+void _AdjustUp(Heap* hp)
+{
+	int j = hp->_size;
+	int i = (j - 1) / 2;
+	while (j > 0)
+	{
+		if (hp->_a[j] < hp->_a[i])
+		{
+			int tmp = hp->_a[j];
+			hp->_a[j] = hp->_a[i];
+			hp->_a[i] = tmp;
+			j = i;
+			i = (j - 1) / 2;
+		}
+		else
+			break;
+	}
+}
+void _AdjustDown(Heap* hp)
+{
+	int i = 0;
+	int j = 2 * i + 1;
+	while (j < hp->_size)
+	{
+		if (j+1<hp->_size && hp->_a[j] > hp->_a[j + 1])
+			j = j + 1;
+
+		if (hp->_a[j] < hp->_a[i])
+		{
+			int tmp = hp->_a[i];
+			hp->_a[i] = hp->_a[j];
+			hp->_a[j] = tmp;
+		}
+		i = j;
+		j = 2 * i + 1;
+	}
+}
+bool HeapIsfull(Heap* hp)
+{
+	return hp->_size >= hp->_capacity;
+}
+int HeapEmpty(Heap* hp)
+{
+	return hp->_size == 0 ? 1 : 0;
+}
+void HeapInit(Heap* hp,int n)
+{
+	hp->_a = (HPDataType*)malloc(sizeof(HPDataType)*n);
+	assert(hp->_a != NULL);
+	hp->_capacity = n;
+	hp->_size = 0;
+
+}
+void HeapShow(Heap* hp)
+{
+	for (int i = 0; i < hp->_size; ++i)
+	{
+		printf("%d ", hp->_a[i]);
+	}
+	printf("\n");
+}
 void HeapCreate(Heap* hp, HPDataType* a, int n)
 {
 
+}
+void HeapPush(Heap* hp, HPDataType x)
+{
+	if (HeapIsfull(hp))
+	{
+		printf("堆已满，%d 不可插入\n", x);
+		return;
+	}
+	hp->_a[hp->_size] = x;
+	_AdjustUp(hp);
+	hp->_size++;
+}
+HPDataType HeapTop(Heap* hp)
+{
+	HPDataType top = hp->_a[0];
+	return top;
+}
+void HeapPop(Heap* hp)
+{
+	if (HeapEmpty(hp))
+	{
+		printf("堆已空，不能删除\n");
+		return;
+	}
+	hp->_size--;
+	hp->_a[0] = hp->_a[hp->_size];
+	_AdjustDown(hp);
+	
 }
 
 
