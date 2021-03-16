d32e37c79cf968ce7460b78b5ea96e7e7f2e273d
mth764 <1242065877@qq.com>
2021-03-16 22:24:39 +0800

    运算符重载

--- C++/Project3_14/Project3_14/data.cpp
+++ C++/Project3_14/Project3_14/data.cpp
@@ -63,6 +63,11 @@ public:
 		}
 		if (_day + day > curday)
 		{
+			if (_month + 1 > 12)
+			{
+				_year += 1;
+				_month = 0;
+			}	
 			_month += 1;
 			day = day - (curday - _day);
 			_day = 0;
@@ -73,34 +78,300 @@ public:
 	// 日期+天数
 	Date operator+(int day)
 	{
-
+		int curday = GetMonthDay(_year, _month);
+		while (day >= curday)
+		{
+			if (_month + 1 > 12)
+			{
+				_year += 1;
+				_month = 0;
+			}
+			_month += 1;
+			day = day - curday;
+			curday = GetMonthDay(_year, _month);
+		}
+		if (_day + day > curday)
+		{
+			if (_month + 1 > 12)
+			{
+				_year += 1;
+				_month = 0;
+			}
+			_month += 1;
+			day = day - (curday - _day);
+			_day = 0;
+		}
+		_day += day;
+		Date tmp(*this);
+		return tmp;
 	}
 	// 日期-天数
-	Date operator-(int day);
+	Date operator-(int day)
+	{
+		if (_month - 1 < 1)
+		{
+			_year -= 1;
+			_month = 13;
+		}
+		int curday = GetMonthDay(_year, _month-1);
+		while (day >= curday)
+		{
+			_month -= 1;
+			day = day - curday;
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			curday = GetMonthDay(_year, _month-1);
+		}
+		if (_day < day )
+		{
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			_month -= 1;
+			day = day-_day;
+			_day = curday;
+		}
+		_day -= day;
+		Date tmp(*this);
+		return tmp;
+	}
 	// 日期-=天数
-	Date& operator-=(int day);
+	Date& operator-=(int day)
+	{
+		if (_month - 1 < 1)
+		{
+			_year -= 1;
+			_month = 13;
+		}
+		int curday = GetMonthDay(_year, _month - 1);
+		while (day >= curday)
+		{
+			_month -= 1;
+			day = day - curday;
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			curday = GetMonthDay(_year, _month - 1);
+		}
+		if (_day < day)
+		{
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			_month -= 1;
+			day = day - _day;
+			_day = curday;
+		}
+		_day -= day;
+		return *this;
+	}
 	// 前置++
-	Date& operator++();
+	Date& operator++()
+	{
+		int curday = GetMonthDay(_year, _month);
+		if (_day + 1 > curday)
+		{
+			if (_month + 1 > 12)
+			{
+				_year += 1;
+				_month = 0;
+				_day = 0;
+			}
+			_month += 1;
+			_day = 0;
+		}
+		_day++;
+		return *this;
+	}
 	// 后置++
-	Date operator++(int);
+	Date operator++(int)
+	{
+		Date tmp(*this);
+		int curday = GetMonthDay(_year, _month);
+		if (_day + 1 > curday)
+		{
+			if (_month + 1 > 12)
+			{
+				_year += 1;
+				_month = 0;
+				_day = 0;
+			}
+			_month += 1;
+			_day = 0;
+		}
+		_day++;
+		return tmp;
+	}
 	// 后置--
-	Date operator--(int);
+	Date operator--(int)
+	{
+		Date tmp(*this);
+		if (_day - 1<1)
+		{
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			int curday = GetMonthDay(_year, _month - 1);
+			_month--;
+			_day = curday + 1;
+		}
+		_day--;
+		return tmp;
+	}
 	// 前置--
-	Date& operator--();
+	Date& operator--()
+	{
+		if (_day - 1<1)
+		{
+			if (_month - 1 < 1)
+			{
+				_year -= 1;
+				_month = 13;
+			}
+			int curday = GetMonthDay(_year, _month - 1);
+			_month--;
+			_day = curday + 1;
+		}
+		_day--;
+		return *this;
+	}
 	// >运算符重载
-	bool operator>(const Date& d);
+	bool operator>(const Date& d)
+	{
+		if (_year == d._year)
+		{
+			if (_month == d._month)
+			{
+				return _day > d._day;
+			}
+			return _month > d._month;
+		}
+		return _year > d._year;
+	}
 	// ==运算符重载
-	bool operator==(const Date& d);
+	bool operator==(const Date& d)
+	{
+		return _year == d._year&&_month == d._month&&_day == d._day;
+	}
 	// >=运算符重载
-	inline bool operator >= (const Date& d);
+	inline bool operator >= (const Date& d)
+	{
+		if (_year == d._year)
+		{
+			if (_month == d._month)
+			{
+				return _day >= d._day;
+			}
+			return _month >= d._month;
+		}
+		return _year >= d._year;
+	}
 	// <运算符重载
-	bool operator < (const Date& d);
+	bool operator < (const Date& d)
+	{
+		if (_year == d._year)
+		{
+			if (_month == d._month)
+			{
+				return _day < d._day;
+			}
+			return _month < d._month;
+		}
+		return _year < d._year;
+	}
 	// <=运算符重载
-	bool operator <= (const Date& d);
+	bool operator <= (const Date& d)
+	{
+		if (_year == d._year)
+		{
+			if (_month == d._month)
+			{
+				return _day <= d._day;
+			}
+			return _month <= d._month;
+		}
+		return _year <= d._year;
+	}
 	// !=运算符重载
-	bool operator != (const Date& d);
+	bool operator != (const Date& d)
+	{
+		return _year != d._year||d._month != d._month||_day != d._day;
+	}
+	int GetYearDay(int year)
+	{
+		if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+		{
+			return 366;
+		}
+		return 365;
+	}
 	// 日期-日期 返回天数
-	int operator-(const Date& d);
+	int operator-(const Date& d)
+	{
+		int sum = 0;
+		if (*this > d)
+		{
+			while (_year > d._year)
+			{
+				int curday = GetYearDay(_year - 1);
+				sum += curday;
+				_year--;
+			}
+			while (_month > d._month)
+			{
+				int curday = GetMonthDay(_year, _month - 1);
+				sum += curday;
+				_month--;
+			}
+			while (_month < d._month)
+			{
+				int curday = GetMonthDay(_year, _month);
+				sum -= curday;
+				_month++;
+			}
+			if (_day>d._day)
+				sum += _day - d._day;
+			else
+				sum -= d._day - _day;
+		}
+		else
+		{
+			while (_year < d._year)
+			{
+				int curday = GetYearDay(_year);
+				sum += curday;
+				_year++;
+			}
+			while (_month > d._month)
+			{
+				int curday = GetMonthDay(_year, _month - 1);
+				sum -= curday;
+				_month--;
+			}
+			while (_month < d._month)
+			{
+				int curday = GetMonthDay(_year, _month);
+				sum += curday;
+				_month++;
+			}
+			if (_day>d._day)
+				sum -= _day - d._day;
+			else
+				sum += d._day - _day;
+		}
+		return sum;
+	}
 private:
 	int _year;
 	int _month;
@@ -113,9 +384,54 @@ ostream& operator<<(ostream& out, const Date& d1)
 }
 int main()
 {
-	Date d0(2021, 3, 15);
-	//Date d1;
-	d0 += 18;//18
-	cout << d0;
+	Date d0(2021, 10, 15);
+	Date d1(2021, 10, 15);
+	Date d2(2021,3,16);
+	Date d3(2021, 3, 16);
+	Date d4(2021, 12, 31);
+	Date d5(1997, 12, 22);//(1998, 5, 22)(1996, 12, 24)(1998, 5, 22)
+	Date d6(1998, 11, 22);//(1998, 5, 22)(1998, 5, 12)(1998, 5, 12)
+	Date d7(2021, 8, 27);
+	Date d8(2018, 7, 27);
+	int t = d7 - d8;
+	cout << t << endl;
+	d0 += 79;//18  79
+	d1 = d1 + 79;//18 79
+	d2 -= 20;//12  20  90
+	d3 = d3 - 20;//12  20  90
+	if (d5 > d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	if (d5 == d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	if (d5 >= d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	if (d5 < d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	if (d5 <= d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	/*if (d5 != d6)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;*/
+	cout << d0 << endl;
+	cout << d1 << endl;
+	cout << d2 << endl;
+	cout << d3 << endl;
+	cout << ++d4 << endl;
+	cout << d4++ << endl;
+	cout << d4 << endl;
+	cout << --d4 << endl;
+	cout << d4-- << endl;
+	cout << d4 << endl;
 	return 0;
 }
